610897980eea465be809bbbf88bc757ccefe27ba
Cawayne Bryan <cawaynebryan@gmail.com>
2022-10-02 11:34:12 -0500

    Start GUI with tkinter

--- db/fertilizer_db.py
+++ db/fertilizer_db.py
@@ -1,4 +1,14 @@
-fertilizer_collections = {
-    "Dap": {"N": 18, "P2O5": 46, "K2O": 0},
-    "Urea": {"N": 46, "P2O5": 0, "K2O": 0},
-}
+fertilizer_collections = [
+    {"formation": "18-46-0", "N": 18, "P2O5": 46, "K2O": 0, "name": "DAP"},
+    {"formation": "12-52-0", "N": 12, "P2O5": 52, "K2O": 0, "name": "MAP"},
+    {"formation": "46-0-0", "N": 46, "P2O5": 0, "K2O": 0, "name": "Urea"},
+    {"formation": "0-0-60", "N": 0, "P2O5": 0, "K2O": 60, "name": "Potash"},
+    {"formation": "82-0-60", "N": 82, "P2O5": 0, "K2O": 60, "name": "Anhydrous Ammonia"},
+    {"formation": "21-0-0", "N": 21, "P2O5": 0, "K2O": 0, "name": "Ammonium Sulphate"},
+    {"formation": "0-45-0", "N": 0, "P2O5": 45, "K2O": 0, "name": "Triple Super Phosphate"},
+    {"formation": "11-22-22", "N": 11, "P2O5": 22, "K2O": 22, "name": ""},
+    {"formation": "14-28-14", "N": 14, "P2O5": 28, "K2O": 14, "name": ""},
+    {"formation": "16-9-18",  "N": 16, "P2O5": 9, "K2O": 18, "name": ""},
+    {"formation": "17-0-17", "N": 17, "P2O5": 0, "K2O": 17, "name": ""},
+    {"formation": "21-0-0",  "N": 21, "P2O5": 0, "K2O": 0, "name": "SOA"},
+]
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import copy, math
-from db import crops_db, fertilizer_db
+# https://aesl.ces.uga.edu/soil/fertcalc/
+import copy
+import math
+from db import crops_db
 
 
 class Crop:
-    """ type: class (object: Crop) Create instance of tyoe Class with object having property -> N,P, K, name, formation and ratio"""
+    """ type: class (object: Crop) Create instance of type Class with object having property
+    -> N,P, K, name, formation and ratio"""
     def __init__(self, name, n=0, p=0, k=0):
         self.N = n
         self.P = p
@@ -25,10 +28,11 @@ class Crop:
 
 
 class Fertilizer(Crop):  # fertilizer object - inherits form Crop
-    """ type: class (object: fertilizer) Create instance of tyoe Class with object having property -> N,P, K, name, formation and ratio"""
+    """ type: class (object: fertilizer) Create instance of type Class with object having property
+     -> N,P, K, name, formation and ratio"""
     def __init__(self, name, n=0, p=0, k=0):
         super().__init__(name, n, p, k)
-        self.weight = self.N + self.P + self.K #TODO: review purpose for this attribute
+        self.weight = self.N + self.P + self.K  # TODO: review purpose for this attribute
 
     def __repr__(self):
         return f'{self.name} + {self.formation}'
@@ -38,59 +42,61 @@ def calculate_fertilizer(crop, *args):
     """" Determine quantity of fertilisers base on crop need """
 
     recommendations = {}
+    requirement_highest_kg = 0
+
     for idx, fertilizer in enumerate(args):
         highest_ratio, middle_ratio, lowest_ratio = fertilizer.sorted()
         highest_npk_key, highest_npk_vale = highest_ratio.popitem()
         middle_npk_key, middle_npk_value = middle_ratio.popitem()
         lowest_npk_key, lowest_npk_value = lowest_ratio.popitem()
 
-        #TODO: if max of fertilizer one is grater than middle of fertilizer 2
+        #   TODO: if max of fertilizer one is grater than middle of fertilizer 2
 
-        if highest_npk_vale != 0:  # Todo: make sure that only fertilizer one is assigned
+        if highest_npk_vale != 0:
             highest_of_ratio = getattr(fertilizer, highest_npk_key)
             requirement_highest_kg = math.floor(getattr(crop, highest_npk_key) * (100/highest_of_ratio))
+            setattr(crop, highest_npk_key, 0)
 
-        recommendations.update({
-            fertilizer.name: requirement_highest_kg,
-        })
+            recommendations.update({
+                fertilizer.name: requirement_highest_kg,
+            })
 
         if middle_ratio != 0:
             middle_of_ratio = getattr(fertilizer, middle_npk_key)
             added_kg_middle = (middle_of_ratio / 100) * requirement_highest_kg
             remaining_middle = math.floor(getattr(crop, middle_npk_key) - added_kg_middle)
+            setattr(crop, middle_npk_key, remaining_middle)
             # print(remaining_middle)  # remaining middle value
 
         if lowest_ratio != 0:
             lowest_of_ratio = getattr(fertilizer, lowest_npk_key)
             added_kg_lowest = (lowest_of_ratio / 100) * requirement_highest_kg
             remaining_lowest = math.floor(getattr(crop, lowest_npk_key) - added_kg_lowest)
+            setattr(crop, lowest_npk_key, remaining_lowest)
             # print(remaining_lowest)  # remaining lowest value
 
         # todo Should only be applied to the first instance
 
-        setattr(crop, highest_npk_key, 0)
-        setattr(crop, middle_npk_key, remaining_middle)
-        setattr(crop, lowest_npk_key, remaining_lowest)
-
     return recommendations
 
 
-def get_selection_by_type(name, type):  # Get crop or fertilizer from db base on the type parameter
+def get_selection_by_type(name, _type):  # Get crop or fertilizer from db base on the type parameter
 
     selected_crop = None
+    selected = None
 
-    if type == 'crop':
+    if _type == 'crop':
         selected = crops_db.crops_collections.get(name.capitalize())
-    if type == 'fertilizer':
+    if _type == 'fertilizer':
         selected = fertilizer_db.fertilizer_collections.get(name.capitalize())
 
     if selected is not None:
         required_n = selected.get('N')
         required_p = selected.get('P2O5')
         required_k = selected.get('K2O')
-        if type == 'crop':
+        if _type == 'crop':
             selected_crop = Crop(name, required_n, required_p, required_k)
-        if type == 'fertilizer':
+        if _type == 'fertilizer':
             selected_crop = Fertilizer(name, required_n, required_p, required_k)
     else:
         print(f'{name} does not exist in database')
@@ -101,9 +107,9 @@ def get_selection_by_type(name, type):  # Get crop or fertilizer from db base on
 
 def main():
 
-    crop = get_selection_by_type(name='rice', type='crop')
-    fertilizer_one = get_selection_by_type(name='DAP', type='fertilizer')
-    fertilizer_two = get_selection_by_type(name='Urea', type='fertilizer')
+    crop = get_selection_by_type(name='corn', _type='crop')
+    fertilizer_one = get_selection_by_type(name='DAP', _type='fertilizer')
+    fertilizer_two = get_selection_by_type(name='Urea', _type='fertilizer')
     recommendations = calculate_fertilizer(
         crop,
         fertilizer_one,
@@ -114,4 +120,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
